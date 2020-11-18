1fd643ebb44917f1da79848dff949846db581c8f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-18 03:19:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
   attr_reader :amount, :sender, :receiver
   attr_accessor :status
 
-  def initalize(sender, receiver, amount)
+  def initialize(sender, receiver, amount)
     @status = "pending"
     @sender = sender
     @receiver = receiver
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending"
-      sender.balnce -= amount
+      sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
     else
@@ -24,4 +24,18 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
